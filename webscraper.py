bcaf65f25ef65cba2d06e52a5b56994c8a323c84
TheStr0m <43961580+TheStr0m@users.noreply.github.com>
2019-04-02 14:55:22 +0200

    infini loop to webscraper

--- webscraper.py
+++ webscraper.py
@@ -1,46 +1,67 @@
+#Importing libraries and declaring variables
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import json
-
+import time
 my_url="https://www.avanza.se/aktier/vinnare-forlorare.html"
-uClient = uReq(my_url)
-page_html = uClient.read()
-uClient.close()
-page_soup= soup(page_html,"html.parser")
-
-stocks=[]
+myStocks=[]
 starttag='['
 endtag=']'
-table= page_soup.select("tbody")[1]
-for row in table.find_all("tr"):
-    priceElement = (row.select("td.lastPrice span"))
-    price = float((priceElement[0].text).replace(",","."))
-    if price>1:
-        nameElement = (row.select("td.instrumentName span a"))
-        name = (nameElement[0].get("title","no title"))
-        percElement = (row.select("td.changePercent"))
-        perc = float((percElement[0].text).replace(",","."))
-        stocks.append([name,perc,price])
-data=open("data.json","w")
-data.write("")
-data.close()
+myJsonFile="data.json"
+myTable=""
 
-for stock in stocks:
-    stockjson={
-        "name":stock[0],
-        "perc":(str(stock[1])+"%"),
-        "price":stock[2],
-    }
+def webLoad(url):
+    global myStocks
+    myStocks=[]
+    uClient = uReq(url)
+    page_html = uClient.read()
+    uClient.close()
+    page_soup= soup(page_html,"html.parser")
+    global myTable
+    myTable= page_soup.select("tbody")[1]
 
-    stockjson=json.dumps(stockjson)
-    data = open("data.json","a+")
-    if stock == stocks[0]:
-        data.write(starttag)
-        data.write(stockjson)
-    elif stock == stocks[-1]:
-        data.write(",\n"+stockjson)
-        data.write(endtag)
-    else:
-        data.write(",\n"+stockjson)
+
+#Picking out interesting data
+def dataPick(table,outputArray):
+    for row in table.find_all("tr"):
+        priceElement = (row.select("td.lastPrice span"))
+        price = float((priceElement[0].text).replace(",","."))
+        if price>1:
+            nameElement = (row.select("td.instrumentName span a"))
+            name = (nameElement[0].get("title","no title"))
+            percElement = (row.select("td.changePercent"))
+            perc = float((percElement[0].text).replace(",","."))
+            global myStocks
+            myStocks.append([name,perc,price])
+
+
+#Writing to JSON
+def writeToJson(jsonFile,array):
+    data=open(jsonFile,"w")
+    data.write("")
     data.close()
+    for stock in array:
+        stockjson={
+            "name":stock[0],
+            "perc":(str(stock[1])+"%"),
+            "price":stock[2],
+        }
+
+        stockjson=json.dumps(stockjson)
+        data = open("data.json","a+")
+        if stock == array[0]:
+            data.write(starttag)
+            data.write(stockjson)
+        elif stock == array[-1]:
+            data.write(",\n"+stockjson)
+            data.write(endtag)
+        else:
+            data.write(",\n"+stockjson)
+        data.close()
+
+while True:
+    webLoad(my_url)
+    dataPick(myTable,myStocks)
+    writeToJson(myJsonFile,myStocks)
+    time.sleep(3)
